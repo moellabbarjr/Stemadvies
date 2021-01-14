0e1c5d2c46c8ab424f6790567a6aede415ef9c26
Leon van den Heuvel <leonheuveltje@gmail.com>
2021-01-14 12:48:58 +0100

    switched to online api. added radiobuttons and next and previous functionality

--- Stemadvies/Views/QuestionsPage.xaml.cs
+++ Stemadvies/Views/QuestionsPage.xaml.cs
@@ -25,10 +25,26 @@ namespace Stemadvies.Views
         public async void GetQuestion()
         {
             var httpClient = new HttpClient();
-            var response = await httpClient.GetStringAsync("http://192.168.2.13/?table=vraag");
+            var response = await httpClient.GetStringAsync("http://535497.student4a8.ao-ica.nl/");
             var question = JsonConvert.DeserializeObject<List <Question> > (response);
+            int maxIndex = question.Count - 1;
+
+            // Check of de index niet onder de nul komt en niet boven de maximale index
+            questionIndex = (questionIndex < 0) ? 0 : questionIndex;
+            questionIndex = (questionIndex > maxIndex) ? maxIndex : questionIndex;
+
+            // Enable en disable buttons op basis van de index
+            previousButton.IsEnabled = (questionIndex <= 0) ? false : true;
+            nextButton.IsEnabled = (questionIndex >= maxIndex) ? false : true;
+
             titleText.Text = $"Vraag {questionIndex + 1} van de {question.Count}";
             questionText.Text = question[questionIndex].vraag;
+
+            answerButtons.Children.Clear();
+            foreach (var answer in question[questionIndex].antwoorden)
+            {
+                answerButtons.Children.Add(new RadioButton { Text = answer.antwoord, GroupName = answer.vraag_id.ToString() });
+            }
         }
 
         private async void NavigateButton(object sender, EventArgs e)
